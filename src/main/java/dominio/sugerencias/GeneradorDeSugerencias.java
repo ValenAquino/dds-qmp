ced084c5765aa04c58aec5a6564356ef249f17bf
ValenAquino <vaquino@frba.utn.edu.ar>
2023-06-11 00:45:19 -0300

    agregar clase abstracta + strategy para los motores de sugerencias

---
+++ src/main/java/dominio/sugerencias/GeneradorDeSugerencias.java
@@ -0,0 +1,33 @@
+package dominio.sugerencias;
+
+import com.google.common.collect.Lists;
+import dominio.Usuario;
+import dominio.atuendos.Atuendo;
+import dominio.atuendos.Prenda;
+import dominio.atuendos.caracteristicas.Categoria;
+import java.util.List;
+import java.util.stream.Collectors;
+
+abstract public class GeneradorDeSugerencias implements MotorDeSugerencias {
+
+  public List<Atuendo> generarSugerencias(Usuario usuario, List<Prenda> guardaRopas) {
+    List<Prenda> prendasValidas = getPrendasValidas(usuario, guardaRopas);
+
+    List<Prenda> superiores = filtrarCategorias(prendasValidas, Categoria.PARTE_SUPERIOR);
+    List<Prenda> inferiores = filtrarCategorias(prendasValidas, Categoria.PARTE_INFERIOR);
+    List<Prenda> calzados = filtrarCategorias(prendasValidas, Categoria.CALZADO);
+
+    List<List<Prenda>> combinaciones = Lists.cartesianProduct(superiores, inferiores, calzados);
+
+    return combinaciones.stream()
+        .map(c -> new Atuendo(c.get(0), c.get(1), c.get(2))).collect(Collectors.toList());
+  }
+
+  public List<Prenda> filtrarCategorias(List<Prenda> prendas, Categoria categoria) {
+    return prendas.stream()
+        .filter(p -> p.getCategoria().equals(categoria)).collect(Collectors.toList());
+  }
+
+  abstract public List<Prenda> getPrendasValidas(Usuario usuario, List<Prenda> prendas);
+
+}
